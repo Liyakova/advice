d57c205c7d0e21eb025688aa6674a9c54f84ec28
Julia <105552026+Liyakova@users.noreply.github.com>
2022-08-18 15:44:42 +0300

    new api

--- src/App.js
+++ src/App.js
@@ -4,25 +4,23 @@ import './App.css';
 function App() {
 
   const [advice, setAdvice] = useState("Code every day!");
-  const [getNew, setGetNew] = useState({advice});
 
-  useEffect( () => {
-    const getAdvice = async () => {
-      const link = "http://www.boredapi.com/api/activity/";
+  const newAdvice = async () => {
+    const link = "https://api.adviceslip.com/advice";
       const response = await fetch(link);
       const result = await response.json();
-      setAdvice(result.activity)
-    }
-    getAdvice();  
-  }, [getNew])
-
-  const newAdvice = () => {
-    setGetNew({advice});
+      setAdvice(result.slip.advice)
   }
+
+  useEffect( () => {
+    newAdvice();  
+  }, [])
+
+
 
   return (
     <div className='container'>
-      <p>{advice}.</p>
+      <p>{advice}</p>
       <button onClick={()=> newAdvice()}>get new advice</button>
     </div>
   );
